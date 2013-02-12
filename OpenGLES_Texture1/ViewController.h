9fa050ea558d3fb9e60e94d43a9f7f675b52678f
Antonio Trejo <atrejo@Antonios-iMac.local>
2013-02-12 16:10:56 -0600

    Initial Commit

---
+++ OpenGLES_Texture1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OpenGLES_Texture1
+//
+//  Created by Antonio Trejo on 2/12/13.
+//  Copyright (c) 2013 Antonio Trejo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ OpenGLES_Texture1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  OpenGLES_Texture1
+//
+//  Created by Antonio Trejo on 2/12/13.
+//  Copyright (c) 2013 Antonio Trejo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
